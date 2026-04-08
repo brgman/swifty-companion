a573e22ca00067ca01535a825e085c817f0f85a7
abergman <abergman@student.42.fr>
2026-04-08 11:38:31 +0000

    feat(*): git stash

--- swifty_companion/lib/components/login_page.dart
+++ swifty_companion/lib/components/login_page.dart
@@ -15,8 +15,6 @@ class LoginPage extends StatlessWidget {
                     apiUrl: "https://api.intra.42.fr/v2/cursus/42/users",
                     userId: "u-s4t2ud-e67ced9ce1d05a02dcea73179b0d7088c9fd95b681cfd5d5d05350bcd589ad3a",
                     secret: "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",
-                    fieldLabel: "OAuth 2.0 API 42",
-                    buttonLabel: "Connect"
                 )
             )
         )
--- swifty_companion/lib/components/oauth_login_form.dart
+++ swifty_companion/lib/components/oauth_login_form.dart
@@ -1,2 +1,44 @@
 import "dart:convert";
-import 
+import "package:flutter/material.dart";
+import "package:http/http.dart" as http;
+
+class OAuthLoginForm extends SatatefulWidget {
+    const OAuthLoginForm({
+        super.key,
+        required this.clientId,
+        required this.clientSecret,
+    })
+
+    final String clientId;
+    final Srinig clientSecret;
+
+    @override State<OAuthLoginForm> createState() => _OAuthLoginForm();
+}
+
+class _OAuthLoginForm extends State<OAuthLoginForm> {
+    bool _loading = false;
+    String? _status;
+    String? _meJson;
+
+    Future<String> _getToken() async {
+        final res = await http.post(
+            Url.parse("https://api.intra.42.fr/oauth/token"),
+            headers: const {
+                'Accept': 'application/json',
+                'Content-Type': 'aplication/x-www-form-urlencoded',
+            },
+            body: {
+                'grand_type': 'client_credentials',
+                'client_id': widget.clientId,
+                'client_secret': widget.clientSecret,
+            },
+        );
+
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+            throw Exception("Token request failed: ${res.statusCode}\n${res.body}");
+        }
+
+        final json = jsonDecode(res.body) as Map<String, dynamic>;
+        final token = json['access_token'] as String?;
+    }
+}
